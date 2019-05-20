a750f80d9c8d94d5e9b4f6a3d31dc1d5b4687627
GuillermoVictorero <45524909+GuillermoVictorero@users.noreply.github.com>
2019-05-20 16:41:10 -0400

    2 Sensors detection and calculation

--- Sistema_de_movimiento_con_Nema_bonito/Sistema_de_movimiento_con_Nema_bonito.ino
+++ Sistema_de_movimiento_con_Nema_bonito/Sistema_de_movimiento_con_Nema_bonito.ino
@@ -6,7 +6,8 @@ const int dirPin=2;
 const int stepPin=3;
 //SENSOR MOVIMIENTO va aqui
 #include <Wire.h>
-#define SIG A0
+#define SIG0 A0
+#define SIG1 A1
 unsigned long rxTime;
 //POSICIONES CARTESIANAS
 #include <math.h>
@@ -15,6 +16,12 @@ double pos1y=0;
 bool pos1=false;
 double pos2x=0;
 double pos2y=0;
+double d0;
+double d1;
+double p0;
+double p1;
+bool eastward;
+double horDist = 15;
 bool pos2=false;
 unsigned long startTime;
 unsigned long endTime;
@@ -25,7 +32,7 @@ double stepAngleRad=1.8*2*PI;
 //DEV
 unsigned long tiempoLectura;
 
-float readDistance(){
+float readDistance(SIG){
   pinMode(SIG, OUTPUT);
     //Genarate a pulse 20uS pulse
     digitalWrite(SIG, HIGH);
@@ -75,8 +82,12 @@ void registerDistance(double distance,double anguloRad){
       endTime=millis();
     }
   }    
-void sensorMovement(){
-  while(!pos2){
+void setSensors(){
+  d0 = readDistance(SIG0);
+  d1 = readDistance(SIG1);
+  
+  
+  /*while(!pos2){
     double distance;
     Serial.print("Pa un lado");
     // Set motor direction clockwise
@@ -106,11 +117,42 @@ void sensorMovement(){
       delayMicroseconds(1000);
       anguloRad-=stepAngleRad;
     }
-  }
+  }*/
 }
+
+void detect(){
+  do{
+    d0 = readDistance(SIG0);
+    d1 = readDistance(SIG1);    
+  }while(d0-p0 > 10 || d1-p1 > 10)
+  startTime=millis();
+  if (d0-p0 > 10){
+    eastward = true;
+    do{
+      d1 = readDistance(SIG1);    
+    }while(d1-p1 > 10)
+    endTime=millis();
+  }
+  else{
+    eastward = false;
+    do{
+      d0 = readDistance(SIG0);    
+    }while(d0-p0 > 10)
+    endTime=millis();
+  }  
+}
+
 void calculations(){
-  double desplazamientoX=pos2x-pos1x;
-  double desplazamientoY=pos2y-pos1y;
+  double desplazamientoX=horDist;
+  double desplazamientoY=p1-p0;
+  pos2x = horDist/2;
+  pos2y = p1;
+  if(!eastward){
+    desplazamientoX= -desplazamientoX;
+    desplazamientoY= -desplazamientoY; 
+    pos2x = -pos2x;
+    pos2y = p0;
+  }
   double elapsedTime=(double)(endTime-startTime)/1000;//tiempo en segundos
   double velocidadX=desplazamientoX/elapsedTime;
   double velocidadY=desplazamientoY/elapsedTime;
@@ -123,7 +165,8 @@ void moveCanon(){
    Serial.println("TERMINO");
 }
 void loop(){
-  sensorMovement();
+  setSensors();
+  detect();
   calculations();
   moveCanon();
 }
